ec7db7d87d28b6c8b9242299554a2fd47036ff96
Ferolinder <julien.maret@isen-ouest.yncrea.fr>
2025-10-13 12:26:56 +0200

    functrioning mine page

--- php/request.php
+++ php/request.php
@@ -12,48 +12,15 @@ include('database.php');
 
     switch($req){
         case "GET" : 
-            if(isset($_GET['action']) && $_GET['action'] == 'getItemsMine'){                // Get all items that can be mined
-                echo json_encode(dbGET_item($db, 1));
-                break;
-            }
-            if(isset($_GET['action']) && $_GET['action'] == 'getMachineMines'){             // Get all machine that can mines
-                echo json_encode(dbGET_machine($db, 1));
-                break;
-            }
-            if(isset($_GET['action']) && $_GET['action'] == 'getMine'){                     // Get all mine
-                echo json_encode(dbGET_mine($db));
-                break;
-            }
-            echo json_encode("Error: unknown GET request");
             break;
         case "POST" : 
-            if(isset($_POST['action']) && $_POST['action'] == 'addMine'){                   // Add a mine
-                // Send the data to the server 
-                dbPOST_mine($db, $_POST);
-                dbPUT_item_qttP($db, $_POST);
-                //Return all the mine (to add the new one)
-                echo json_encode(dbGET_mine($db));
-            }
             break;
         case "PUT" : // Update
             // Parse PUT data from input stream
             parse_str(file_get_contents("php://input"), $_PUT);
-            if(isset($_PUT['action']) && $_PUT['action'] == 'updateMine'){      // Update a mine
-                // Updating the data 
-                dbPUT_mine($db, $_PUT);
-                dbPUT_item_qttP($db,$_PUT);
-                // Return all the mine
-                echo json_encode(dbGET_mine($db));
-            }
             break;
         case "DELETE" :
-            parse_str(file_get_contents("php://input"), $params);
-            if(isset($params['action']) && $params['action'] == 'deleteMine'){      // Delete a mine
-                // Deleting the data 
-                dbDELETE_mine($db,$params);
-                // Return all the mine 
-                echo json_encode(dbGET_mine($db));
-            }
+            parse_str(file_get_contents("php://input"), $_DELETE);
             break;
     }
 
